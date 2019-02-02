acec81d68c12fd413deffb6c12230264aec145b1
Kevin Machado <machadokevin.dev@gmail.com>
2019-02-02 17:11:14 +0100

    Swift PackageManager implemented

--- Tests/RxGramAPITests/RxGramAPITests.swift
+++ Tests/RxGramAPITests/RxGramAPITests.swift
@@ -11,13 +11,11 @@ import XCTest
 import RxGramAPI
 
 class RxGramAPITests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        //// XCTAssertEqual(RxGramAPI().text, "Hello, World!")
+    func testAlwaysSuccess() {
+         XCTAssert(true)
     }
     
     static var allTests = [
-        ("testExample", testExample),
+        ("testAlwaysSuccess", testAlwaysSuccess),
     ]
 }
